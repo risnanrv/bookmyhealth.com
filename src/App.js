4a756f74fdeffe18f3ef2f9484ee7c0097022dc9
blackshader01 <risnanrv.1877@gmail.com>
2025-01-11 23:51:03 +0530

    Main header added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import MainHeader from './Components/MainHeader/MainHeader';
 import NavSection from './Components/Navbar/NavSection';
 
 function App() {
   return (
     <div className="App">
     <NavSection/>
+    <MainHeader/>
     </div>
   );
 }
---
+++ src/Components/MainHeader/MainHeader.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './MainHeader.css'
+import { Container, Col} from 'react-bootstrap';
+import HeaderImage from '../../Assets/header_img.png'
+import { FaLongArrowAltRight } from "react-icons/fa";
+function MainHeader() {
+  return (
+    <Container className='header'>
+        
+        <div className="Row main">
+        <Col md={6} xs={12} className="content">
+        <h3>
+            Connect With Expert <br></br> Healthcare Professionals
+        </h3>
+        <p>Browse a curated selection of expert healthcare professionals and book appointments easily, tailored to your needs.</p>
+        <button className='appoinment-button'>Book Now <FaLongArrowAltRight /></button>
+
+        </Col>
+        <Col md={6} xs={12} className="image">
+            <img src={HeaderImage} alt='HeaderImage' className='header-image'></img>
+            
+        </Col>
+        </div>
+    </Container>
+  )
+}
+
+export default MainHeader
